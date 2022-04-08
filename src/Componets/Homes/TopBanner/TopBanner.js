756a7d2878d929d5e86a400da43ff92b2e2f826b
sabbir-hossai <sabbirab3@gmail.com>
2022-04-08 11:02:20 +0600

    top banner done

--- src/Componets/Homes/TopBanner/TopBanner.js
+++ src/Componets/Homes/TopBanner/TopBanner.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './TopBanner.css';
-import backgroundImg from '../../../images/main bg .png';
+import mainImg from '../../../images/main bg .png';
+import mainBg from '../../../images/galax.png';
+import mainBg2 from '../../../images/Vector 98.png';
+import box from '../../../images/3 1.png';
+import ring from '../../../images/ring.png';
+import ston from '../../../images/Asset 5@2x-8.png';
 
 const TopBanner = () => {
     return (
-        <div className='topBanner'>
-            <div className='container '>
-                <img className='' src={backgroundImg} alt="" />
+        <div className=''>
+            <div className='topBanner  '>
+                <div className='mainImg  '>
+                    <img className='' src={mainImg} alt="" />
+                </div>
+                <div className='mainBg'>
+                    <img className='' src={mainBg} alt="" />
+                </div>
+                <div className='mainBg2'>
+                    <img className='' src={mainBg2} alt="" />
+                </div>
+                <div className='box'>
+                    <img className='' src={box} alt="" />
+                </div>
+                <div className='ring'>
+                    <img className='' src={ring} alt="" />
+                </div>
+                <div className='ston'>
+                    <img className='' src={ston} alt="" />
+                </div>
+
+                <img src="" alt="" />
             </div>
 
         </div>
--- src/Componets/Homes/nabvar/Navbar.js
+++ src/Componets/Homes/nabvar/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light  ">
                     <div className="container">
-                        <img src={navIcon} className='m-4' alt="" />
+                        <img src={navIcon} className='me-4' alt="" />
                         <h1>Space</h1>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
