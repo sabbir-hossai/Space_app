cbc1c7ebc347d82aa74b38bcea8d3b170c645fe0
sabbir-hossai <sabbirab3@gmail.com>
2022-04-10 22:32:44 +0600

    done

--- src/Componets/Homes/MainProducts/MainProducts.js
+++ src/Componets/Homes/MainProducts/MainProducts.js
@@ -58,6 +58,26 @@ const MainProducts = () => {
                 }
 
             </div>
+            <nav aria-label="Page navigation example">
+                <ul className="pagination d-flex justify-content-center mt-5">
+                    <li className="page-item">
+                        <a className="page-link" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                    <li className="page-item"><a className="page-link" href="#">1</a></li>
+                    <li className="page-item"><a className="page-link" href="#">2</a></li>
+                    <li className="page-item"><a className="page-link" href="#">3</a></li>
+                    <li className="page-item"><a className="page-link" href="#">4</a></li>
+                    <li className="page-item"><a className="page-link" href="#">5</a></li>
+                    <li className="page-item"><a className="page-link" href="#">6</a></li>
+                    <li className="page-item">
+                        <a className="page-link" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
 
         </div>
     );
